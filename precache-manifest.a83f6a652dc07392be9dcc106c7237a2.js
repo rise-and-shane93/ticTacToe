47cc4631e32e4154ec5af846fe1bc2dc6e8aea4a
Shane Harper <cdtharper@gmail.com>
2020-03-28 17:16:16 -0400

    Updates

---
+++ precache-manifest.a83f6a652dc07392be9dcc106c7237a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbc20dd018cedef9edacc92242281ac",
+    "url": "/ticTacToe/index.html"
+  },
+  {
+    "revision": "2938e1e006c68bc8bd4d",
+    "url": "/ticTacToe/static/css/main.4b5241f0.chunk.css"
+  },
+  {
+    "revision": "1d579012ffbd732d4b90",
+    "url": "/ticTacToe/static/js/2.2c472d56.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ticTacToe/static/js/2.2c472d56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2938e1e006c68bc8bd4d",
+    "url": "/ticTacToe/static/js/main.ddf66c7b.chunk.js"
+  },
+  {
+    "revision": "f6d506568977ad73188a",
+    "url": "/ticTacToe/static/js/runtime-main.b26fcaba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ticTacToe/static/media/logo.5d5d9eef.svg"
+  }
+]);
